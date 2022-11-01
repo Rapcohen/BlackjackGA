aae08c144c211822790aded68807728fad8df798
Raphael <rapcohen@gmail.com>
2022-11-01 22:37:12 +0200

    Blackjack Implementation

---
+++ blackjack.py
@@ -0,0 +1,100 @@
+import random
+from enum import Enum
+from typing import List
+
+from blackjack21 import Table
+from blackjack21.deck import Card
+from blackjack21.players import Player
+
+
+class Action(Enum):
+    HIT = 1
+    STAND = 2
+    DOUBLE_DOWN = 3
+    # SPLIT = 4
+
+
+class Result(Enum):
+    PLAYER_BUST = -2
+    PLAYER_LOSE = -1
+    TIE = 0
+    PLAYER_BLACKJACK = 1
+    PLAYER_WIN = 2
+    DEALER_BUST = 3
+
+
+class Strategy:
+    def __init__(self, hard_hands: List[List[Action]], soft_hands: List[List[Action]]):
+        self.hard_hands = hard_hands
+        self.soft_hands = soft_hands
+        self.min_hard_hand = 4
+        self.min_soft_hand_without_ace = 2
+        self.min_dealer_hand = 2
+
+    def get_action(self, dealer_card: Card, player: Player) -> Action:
+        if self.is_soft(player.hand):
+            action = self.get_soft_action(dealer_card, player.hand)
+        else:
+            action = self.get_hard_action(dealer_card, player)
+        if action == Action.DOUBLE_DOWN and len(player.hand) > 2:
+            action = Action.HIT
+        return action
+
+    def is_soft(self, hand: List[Card]):
+        has_ace = any(card.rank == "A" for card in hand)
+        return has_ace and sum([card._value for card in hand]) <= 20
+
+    def get_hard_action(self, dealer_card: Card, player: Player) -> Action:
+        return self.hard_hands[dealer_card._value - self.min_dealer_hand][player.total - self.min_hard_hand]
+
+    def get_soft_action(self, dealer_card: Card, player_hand: List[Card]) -> Action:
+        hand_value_without_ace = sum([card._value for card in player_hand if card.rank != "A"])
+        return self.soft_hands[dealer_card._value - self.min_dealer_hand][
+            hand_value_without_ace - self.min_soft_hand_without_ace]
+
+
+def simulate_hands(n_hands: int, strategy):
+    fixed_bet = 1
+    cash = 0
+
+    for hand in range(n_hands):
+        table = Table((("AI", fixed_bet),))
+        player = table.players[0]
+        dealer_hand = table.dealer.hand[0]
+
+        if player.total == 21:
+            continue
+
+        action = strategy.get_action(dealer_hand, player)
+        if action == Action.DOUBLE_DOWN:
+            player.play_double_down()
+
+        while not (player.bust or player.stand):
+            action = strategy.get_action(dealer_hand, player)
+            if action == Action.HIT:
+                player.play_hit()
+            elif action == Action.STAND:
+                player.play_stand()
+            # elif action == Action.SPLIT and player.can_split:
+            #     player.play_split()
+
+        table.dealer.play_dealer()
+        result = Result(player.result)
+        if result in [Result.PLAYER_BLACKJACK, Result.PLAYER_WIN, Result.DEALER_BUST]:
+            cash += player.bet
+        elif result in [Result.PLAYER_BUST, Result.PLAYER_LOSE]:
+            cash -= player.bet
+
+    return cash
+
+
+def random_action():
+    return random.choice(list(Action))
+
+
+if __name__ == "__main__":
+    strategy = Strategy(
+        [[random_action() for _ in range(17)] for _ in range(10)],
+        [[random_action() for _ in range(8)] for _ in range(10)],
+    )
+    print(simulate_hands(100000, strategy))
--- main.py
+++ main.py
@@ -1,33 +1,56 @@
+import random
+
 from eckity.algorithms.simple_evolution import SimpleEvolution
 from eckity.creators.creator import Creator
 from eckity.evaluators.simple_individual_evaluator import SimpleIndividualEvaluator
+from eckity.fitness.fitness import Fitness
+from eckity.fitness.simple_fitness import SimpleFitness
 from eckity.genetic_operators.genetic_operator import GeneticOperator
 from eckity.genetic_operators.selections.tournament_selection import TournamentSelection
+from eckity.individual import Individual
 from eckity.subpopulation import Subpopulation
+
+from blackjack import Strategy, simulate_hands, Action
+
+
+class StrategyIndividual(Individual):
+    def __init__(self, strategy: Strategy, fitness: Fitness):
+        super().__init__(fitness)
+        self.strategy = strategy
 
 
 class StrategyEvaluator(SimpleIndividualEvaluator):
-    def _evaluate_individual(self, individual):
+    def _evaluate_individual(self, individual: StrategyIndividual):
         # simulate N hands using this strategy and return the normalized chip count (non-negative)
-        pass
+        return simulate_hands(1000, individual.strategy)
 
 
 class StrategyCreator(Creator):
+    def random_action(self):
+        return random.choice(list(Action))
+
     def create_individuals(self, n_individuals, higher_is_better):
         # create n individuals with random strategies
-        pass
+        ivdividuals = []
+        for _ in range(n_individuals):
+            strategy = Strategy(
+                [[self.random_action() for _ in range(17)] for _ in range(10)],
+                [[self.random_action() for _ in range(8)] for _ in range(10)],
+            )
+            ivdividuals.append(StrategyIndividual(strategy, SimpleFitness(higher_is_better=higher_is_better)))
+        return ivdividuals
 
 
 class StrategyCrossover(GeneticOperator):
     def apply(self, individuals):
         # apply crossover to the individuals (relative to their fitness)
-        pass
+        return individuals
 
 
 class StrategyMutation(GeneticOperator):
     def apply(self, individuals):
         # apply mutation to the individuals
-        pass
+        return individuals
 
 
 algo = SimpleEvolution(
@@ -48,8 +71,9 @@ algo = SimpleEvolution(
         higher_is_better=True
     ),
     statistics=None,
-    max_generation=100  # find optimal value
+    max_generation=10  # find optimal value
 )
 
-algo.evolve()
-algo.finish()
+if __name__ == "__main__":
+    algo.evolve()
+    algo.finish()
