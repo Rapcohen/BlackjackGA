8b6805fc1d29c61492765f02ed655af4ddada87e
Raphael <rapcohen@gmail.com>
2022-12-10 18:37:19 +0200

    Cleanup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import time
-
 from ga import evolution_algo
 
 if __name__ == "__main__":
-    start_time = time.time()
     evolution_algo().evolve()
-    print("--- execution time: %.2f seconds ---" % (time.time() - start_time))
